2bb32a35f55e6f37c4b480201d052677708a1033
Adarsh kumar singh <adi23052002@gmail.com>
2026-06-09 13:35:11 +0530

    Week Name

---
+++ WeekdayName.py
@@ -0,0 +1,19 @@
+day = 0  # Change this value (0-6) to test other days
+
+if day == 0:
+    print('Sunday')
+elif day == 1:
+    print('Monday')
+elif day == 2:
+    print('tuesday')
+elif day == 3:
+    print('wednesday')
+elif day == 4:
+    print('thrusday')
+elif day == 5:
+    print('friday')
+elif day == 6:
+    print('saturday')    
+#Continue the elifs for other Days
+else:
+    print('Invalid Day Number')
