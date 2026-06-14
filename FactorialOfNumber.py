8692e16f372e324d0f43e19b702281ba48cc39c1
Adarsh kumar singh <adi23052002@gmail.com>
2026-06-14 14:17:58 +0530

    Practice code

---
+++ FactorialOfNumber.py
@@ -0,0 +1,8 @@
+n =int(input('Enter a Number:'))
+fact = 1
+
+for i in range(1,n+1):
+    fact = fact*i
+    #TODO : Write the code logic to calculate the Factorial of a given number using for loop below
+
+print('Factorial of',n,'is', fact)
---
+++ FibonacciSeries.py
@@ -0,0 +1,11 @@
+n =int(input('Enter the Number:')) # You can change this value to test other cases
+
+a = 0
+b = 1
+
+for i in range(0,n):
+    c=a+b
+    a=b
+    b=c#TODO:Write the code logic for Fibonacci series using for loop below
+
+print(a)
